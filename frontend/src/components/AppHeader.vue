9e269725ae863da35a728c8d7eca940a9eb19622
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-06 20:28:07 -0700

    Adds AppHeader

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="app-header">
+    <h1>Recipe Hero</h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/_typography.scss";
+@import "@/styles/_sizes.scss";
+
+.app-header {
+  padding: $xl 0;
+  h1 {
+    @include heading-font-1;
+    text-align: center;
+  }
+}
+</style>
