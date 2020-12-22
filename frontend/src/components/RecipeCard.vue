9ecbb85655e4fd21b6e287115fdd9ff77aad3157
Scott Golaszewski <sgola37@gmail.com>
2020-12-21 20:06:15 -0700

    Enable recipe steps.

--- frontend/src/components/RecipeCard.vue
+++ frontend/src/components/RecipeCard.vue
@@ -8,7 +8,9 @@
     <RecipeStepIndicator :numberOfSteps="numberOfSteps" :activeStep="activeStep" />
     <RecipeStep :step="currentStep" />
     <div class="command-bar">
-      <!-- Placeholder to keep flex style --><span></span><button>Next</button>
+      <button v-if="activeStep > 1" @click="previousStep">Previous</button>
+      <span v-else></span>
+      <button v-if="activeStep < numberOfSteps" @click="nextStep">Next</button>
     </div>
   </div>
 </template>
@@ -42,6 +44,18 @@ export default {
     },
     currentStep() {
       return this.recipe.steps[this.activeStep - 1];
+    },
+  },
+  methods: {
+    nextStep() {
+      if (this.activeStep < this.numberOfSteps) {
+        this.activeStep += 1;
+      }
+    },
+    previousStep() {
+      if (this.activeStep > 1) {
+        this.activeStep -= 1;
+      }
     },
   },
 };
